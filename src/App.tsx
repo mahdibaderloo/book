c8dda728bf49c9087219689a8fcfe71b0e68ec01
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-01 23:03:57 +0330

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    element: <></>,
+    children: [
+      { path: "/", element: <></> },
+      { path: "/shopping-cart", element: <></> },
+      { path: "/login", element: <></> },
+      { path: "/signup", element: <></> },
+    ],
+  },
+]);
+
 function App() {
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
