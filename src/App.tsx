5271646899f611f19d6ccb65e3b24230224fed0c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-01 22:57:10 +0330

    install react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 function App() {
-  return <></>;
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
