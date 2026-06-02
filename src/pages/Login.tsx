39faac2a143542bcc2ddb85a718485c9df7b9408
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-02 21:51:57 +0330

    Login styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,16 @@ import backgroundImg from "../assets/background.png";
 import booksImg from "../assets/books.png";
 import { TbLockPassword } from "react-icons/tb";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handleShowPasswordToggle() {
+    setShowPassword((s) => !s);
+  }
+
   return (
     <div
       className="h-screen relative overflow-hidden"
@@ -13,7 +21,7 @@ export default function Login() {
       <div className="bg-black h-screen opacity-80" />
       <form className="absolute top-50 left-1/2 -translate-x-1/2 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg flex flex-col justify-center items-center p-4 w-90">
         <img src={booksImg} alt="لوگو" className="w-24" />
-        <p className="text-white font-bold text-3xl">صفحه ورود</p>
+        <p className="text-white font-bold text-3xl">ورود به حساب کابری</p>
         <div className="flex gap-1 bg-gray-300 rounded-xl p-2 items-center mt-8 w-[90%]">
           <input
             type="email"
@@ -24,16 +32,34 @@ export default function Login() {
           <HiOutlineMail className="text-2xl text-gray-700" />
         </div>
         <div className="flex gap-1 bg-gray-300 rounded-xl p-2 items-center mt-4 w-[90%]">
-          <FaRegEye className="text-xl text-gray-700 cursor-pointer" />
-          {/* <FaRegEyeSlash className="text-xl text-gray-700 cursor-pointer" /> */}
+          {showPassword ? (
+            <FaRegEye
+              className="text-xl text-gray-700 cursor-pointer"
+              onClick={handleShowPasswordToggle}
+            />
+          ) : (
+            <FaRegEyeSlash
+              className="text-xl text-gray-700 cursor-pointer"
+              onClick={handleShowPasswordToggle}
+            />
+          )}
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password"
             dir="ltr"
             className="border-none outline-none py-1 w-full font-medium"
           />
           <TbLockPassword className="text-2xl text-gray-700" />
         </div>
+        <p className="mt-2 text-sm">
+          <span className="text-gray-400">حساب کاربری نداری؟</span>
+          <Link
+            to="/signup"
+            className="mr-2 underline text-gray-400 hover:text-gray-50 transition-all duration-400"
+          >
+            ثبت نام
+          </Link>
+        </p>
         <button className="flex items-center justify-center bg-gray-950 hover:bg-gray-900 hover:shadow transition-all duration-400 text-gray-50 rounded-xl p-2 pb-3 w-[90%] mt-10 cursor-pointer font-medium text-lg">
           <span>ورود</span>
         </button>
