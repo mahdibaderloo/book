923d90668d631a1801ac9e0c0b0f89f16f37bbc7
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-03 21:03:56 +0330

    scroll to products button

--- src/features/home/Banner.tsx
+++ src/features/home/Banner.tsx
@@ -2,13 +2,22 @@ import { FaRegArrowAltCircleDown } from "react-icons/fa";
 import bannerImg from "../../assets/books.png";
 
 export default function Banner() {
+  function handleClick() {
+    document.getElementById("products")?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
   return (
     <section className="w-full bg-orange-300 h-60 flex justify-between items-center max-w-6xl mx-auto">
       <img src={bannerImg} alt="books" className="w-60 mr-14" />
       <div className="ml-20 text-2xl font-bold">
         <p className="text-orange-50">کـتـابـی کــــه دنبالــش بودی</p>
         <p className="text-orange-50">یه قدم باهاش فاصله داری</p>
-        <button className="bg-orange-100 hover:bg-orange-50 transition-all duration-400 hover:shadow flex justify-center items-center gap-1 mt-4 text-[0.75rem] font-medium px-2 py-1 justify-self-end rounded-2xl cursor-pointer">
+        <button
+          className="bg-orange-100 hover:bg-orange-50 transition-all duration-400 hover:shadow flex justify-center items-center gap-1 mt-4 text-[0.75rem] font-medium px-2 py-1 justify-self-end rounded-2xl cursor-pointer"
+          onClick={handleClick}
+        >
           <span>مشاهده محصولات</span>
           <FaRegArrowAltCircleDown />
         </button>
--- src/features/home/Products.tsx
+++ src/features/home/Products.tsx
@@ -2,7 +2,7 @@ import ProductItem from "./ProductItem";
 
 export default function Products() {
   return (
-    <div className="mt-4 p-4 max-w-6xl mx-auto">
+    <div className="mt-4 p-4 max-w-6xl mx-auto scroll-mt-24" id="products">
       <p className="border-r-4 border-orange-300 pb-1 pr-1 text-sm font-semibold">
         محصولات
       </p>
