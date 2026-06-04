0c68846b3421a669727e048527845c941a7049d1
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-04 20:52:45 +0330

    handle navigate to product detail

--- frontend/src/features/home/ProductItem.tsx
+++ frontend/src/features/home/ProductItem.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import type { Book } from "../../types/types";
 
 export default function ProductItem({ book }: { book: Book }) {
+  const navigate = useNavigate();
+
+  function handleClickOnImage() {
+    navigate(`/book/${book.id}`);
+  }
+
   return (
     <li className="flex flex-col p-2 rounded-lg overflow-hidden shadow-md bg-white hover:scale-[1.02] transition-all">
       <div className="w-36 h-36 bg-gray-50 shadow rounded-lg overflow-hidden cursor-pointer">
-        <img src={book.imageUrl} alt="" />
+        <img
+          src={book.imageUrl}
+          alt="تصویر محصول"
+          onClick={handleClickOnImage}
+        />
       </div>
       <p className="text-[0.8rem] font-medium mt-2">{book.title}</p>
       <span className="text-[0.8rem] font-medium mt-2">
