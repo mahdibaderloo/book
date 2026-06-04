02f977efbfa32841232a8a7a1071cacdcf7f266b
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-04 12:13:51 +0330

    add backend with spring

--- src/components/FooterCopyrightSection.tsx
+++
@@ -1,9 +0,0 @@
-export default function FooterCopyrightSection() {
-  return (
-    <section className="mt-4 flex justify-end">
-      <span className="justify-self-end text-[0.5rem] font-medium text-gray-600">
-        BookStore2026©
-      </span>
-    </section>
-  );
-}
--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import loadingGif from "../assets/loading.gif";
-
-export default function Loading() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <img src={loadingGif} alt="loading..." />
-    </div>
-  );
-}
