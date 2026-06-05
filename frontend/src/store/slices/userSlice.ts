cd1570780a3e952c58dc3d357b99bc8637bf0d4f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-05 16:58:47 +0330

    add userSlice

---
+++ frontend/src/store/slices/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+  user: User | null;
+}
+
+const initialState: UserState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+
+    logout(state) {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logout } = userSlice.actions;
+
+export default userSlice.reducer;
