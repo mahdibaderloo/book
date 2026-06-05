d1bc4d954bfd016d50e4f0a015795f6eeb8f8778
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-05 10:05:11 +0330

    add cartSlice

---
+++ frontend/src/store/cartSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    setCart(state, action) {
+      state.items = action.payload;
+    },
+
+    addItem(state, action) {
+      state.items.push(action.payload);
+    },
+
+    removeItem(state, action) {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+
+    clearCart(state) {
+      state.items = [];
+    },
+  },
+});
+
+export const { setCart, addItem, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
