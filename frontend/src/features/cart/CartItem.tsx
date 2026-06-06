141da473eb58e2fef7f237d217a8fad761e3b7c4
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-06 17:24:17 +0330

    CartItem styles

--- frontend/src/features/cart/CartItem.tsx
+++ frontend/src/features/cart/CartItem.tsx
@@ -9,15 +9,17 @@ interface Item extends Book {
 export default function CartItem({ item }: { item: Item }) {
   return (
     <li className="w-[80%] mx-auto flex justify-between items-center pt-2 pb-4">
-      <div className="flex gap-2">
+      <div className="flex gap-4">
         <img
           src={item.imageUrl}
           alt="محصول"
           className="w-24 h-24 bg-amber-200 rounded-xl shadow"
         />
         <div className="flex flex-col justify-between py-4">
-          <p className="font-medium text-sm">{item.title}</p>
-          <span className="text-[0.9rem]">{item.price.toLocaleString()}</span>
+          <p className="font-medium text-md">{item.title}</p>
+          <span className="text-[0.9rem] font-medium">
+            {item.price.toLocaleString()}
+          </span>
         </div>
       </div>
       <div className="flex gap-2">
@@ -29,7 +31,9 @@ export default function CartItem({ item }: { item: Item }) {
           <TiMinus />
         </button>
       </div>
-      <span className="font-medium">{item.quantity * item.price}</span>
+      <span className="font-medium">
+        {(item.quantity * item.price).toLocaleString()}
+      </span>
       <FaTrashCan className="text-orange-400 cursor-pointer" />
     </li>
   );
