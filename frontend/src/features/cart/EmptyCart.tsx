6ee97f1d9bd70215fe7a0d51457ffae61e22ba70
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-06-06 21:16:37 +0330

    EmptyCart styles

--- frontend/src/features/cart/EmptyCart.tsx
+++ frontend/src/features/cart/EmptyCart.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 
 export default function EmptyCart() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col w-full justify-center items-center mt-24">
       <p className="text-2xl font-semibold text-orange-600">
         سبد خرید شما خالیه!
       </p>
-      <Button title="مشاهده محصولات" width="90" mt="4" />
+      <Button
+        title="مشاهده محصولات"
+        width="90"
+        mt="4"
+        onClick={() => navigate("/")}
+      />
     </div>
   );
 }
--- frontend/src/features/cart/Total.tsx
+++ frontend/src/features/cart/Total.tsx
@@ -1,6 +1,10 @@
 import Button from "../../components/Button";
 
 export default function Total({ total }: { total: number }) {
+  if (total === 0) {
+    return null;
+  }
+
   return (
     <div className="p-4 border-t-2 border-gray-300 w-full flex flex-col justify-center items-center">
       <div className="flex justify-between items-center px-2 w-84 font-bold">
