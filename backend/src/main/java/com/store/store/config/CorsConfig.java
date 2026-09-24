b44ea90333e9ec921cdbf3a75a46f5a13748ff32
Mahdi <mahdi.baderloo.230@gmail.com>
2026-09-24 21:46:54 +0330

    cors config

--- backend/src/main/java/com/store/store/config/CorsConfig.java
+++ backend/src/main/java/com/store/store/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://book-3mki.onrender.com")
+                .allowedOrigins("https://book-front-one.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
